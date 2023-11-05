ee285662e9aefc6da9d3918f475ce0acfdcf694d
Wágner Bánk <wagner.bank@easy-way.hu>
2023-11-05 18:59:27 +0100

    feat: added header, styles bug: primeng tabmenu doesnt work

--- restaurant/src/app/app.component.ts
+++ restaurant/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from './components/Header/header.component';
 
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
