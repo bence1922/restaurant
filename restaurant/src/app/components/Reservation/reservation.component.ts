1275e4650928443ed5a4df18dfb7ffc9a4df0399
dorinaorova <dorina.orova84@gmail.com>
2023-11-06 22:49:30 +0100

    reservation done

--- restaurant/src/app/components/Reservation/reservation.component.ts
+++ restaurant/src/app/components/Reservation/reservation.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroComponent } from '../Hero/hero.component';
+import { DividerModule } from 'primeng/divider';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-reservation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeroComponent, DividerModule, CalendarModule],
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.scss'],
 })
-export class ReservationComponent {}
+export class ReservationComponent {
+  date: Date | undefined
+}
