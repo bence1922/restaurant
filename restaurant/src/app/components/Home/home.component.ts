f4e74c890bf5adab6f3ac655099b9df14c89c408
Wágner Bánk <wagner.bank@easy-way.hu>
2023-11-08 19:17:30 +0100

    feat: home component design practicly done

--- restaurant/src/app/components/Home/home.component.ts
+++ restaurant/src/app/components/Home/home.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../Hero/hero.component';
+import { ImageModule } from 'primeng/image';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { SplitterModule } from 'primeng/splitter';
+import { GalleriaModule } from 'primeng/galleria';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeroComponent],
+  imports: [CommonModule, HeroComponent, ImageModule, CardModule, ButtonModule, SplitterModule, GalleriaModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  // Image gallery: https://primeng.org/galleria
+
+  images: any[] | undefined;
+  responsiveOptions: any[] | undefined;
+
+  ngOnInit(){
+    this.responsiveOptions = [
+      {
+          breakpoint: '1024px',
+          numVisible: 5,
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 3,
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1,
+      }
+  ];
+  }
+
+
+}
