4e61e41cebdaac24a08cb36fdb2c0e506f7a4808
Wágner Bánk <wagner.bank@easy-way.hu>
2023-11-09 22:56:17 +0100

    feat: about us page done

--- restaurant/src/app/components/AboutUs/about-us.component.ts
+++ restaurant/src/app/components/AboutUs/about-us.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroComponent } from '../Hero/hero.component';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeroComponent, ImageModule],
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
 })
