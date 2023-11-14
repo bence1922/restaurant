06659f32cd448d11a6ebf180549625358c5c5d8a
dorinaorova <dorina.orova84@gmail.com>
2023-11-14 20:08:53 +0100

    reservation page: done, form not working

--- restaurant/src/app/components/Reservation/reservation.component.ts
+++ restaurant/src/app/components/Reservation/reservation.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../Hero/hero.component';
 import { DividerModule } from 'primeng/divider';
 import { CalendarModule } from 'primeng/calendar';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation',
@@ -11,6 +12,25 @@ import { CalendarModule } from 'primeng/calendar';
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.scss'],
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit {
   date: Date | undefined
+
+  newReservationForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    date: new FormControl('', [Validators.required]),
+    time: new FormControl('', [Validators.required]),
+    guests: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    note: new FormControl(''),
+  })
+
+  onSubmit(){
+    console.log("haloooo")
+  }
+
+  constructor(){}
+
+  ngOnInit(){
+  }
 }
