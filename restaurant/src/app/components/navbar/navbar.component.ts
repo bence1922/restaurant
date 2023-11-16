005824aca941d5402df8135baafb3410cc2d0918
Wágner Bánk <wagner.bank@easy-way.hu>
2023-11-16 17:11:26 +0100

    feat: navbar jogköröket kezeli

--- restaurant/src/app/components/navbar/navbar.component.ts
+++ restaurant/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from 'src/app/generated-api/model/user';
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +9,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  user: User | undefined;
+
+  constructor() {
+    if(!this.user || this.user?.role=="customer"){
+      console.log("customer");
+    }
+    if(this.user?.role=="manager" || this.user?.role=="waiter"){
+      console.log("employee");
+    }
+  }
+
+}
