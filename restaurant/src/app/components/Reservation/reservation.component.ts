4eed718b62f253f2e3970d1c75f78829d7225338
dorinaorova <dorina.orova84@gmail.com>
2023-11-25 17:55:49 +0100

    make reservation form, table form

--- restaurant/src/app/components/Reservation/reservation.component.ts
+++ restaurant/src/app/components/Reservation/reservation.component.ts
@@ -3,33 +3,78 @@ import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../Hero/hero.component';
 import { DividerModule } from 'primeng/divider';
 import { CalendarModule } from 'primeng/calendar';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BookingService, NewBooking } from 'src/app/generated-api';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation',
   standalone: true,
-  imports: [CommonModule, HeroComponent, DividerModule, CalendarModule],
+  imports: [CommonModule, HeroComponent, DividerModule, CalendarModule, FormsModule, ReactiveFormsModule],
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.scss'],
 })
 export class ReservationComponent implements OnInit {
   date: Date | undefined
-
   newReservationForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     date: new FormControl('', [Validators.required]),
     time: new FormControl('', [Validators.required]),
-    guests: new FormControl('', [Validators.required]),
+    guests: new FormControl(0, [Validators.required]),
     email: new FormControl('', [Validators.required]),
     phone: new FormControl('', [Validators.required]),
     note: new FormControl(''),
   })
+  
+  constructor(
+    private bookingService: BookingService,
+    private router: Router
+  ) {}
 
   onSubmit(){
-    console.log("haloooo")
+    var customerId = '1'  
+    var data: NewBooking={
+      tableNumber: 1, //TODO
+      user_id: customerId,
+      startingDate: new Date(this.newReservationForm.value.date!).toString(),
+      endingDate: "",
+      peopleCount: this.newReservationForm.value.guests!
+    }
+
+    // this.bookingService.bookTable(data).subscribe(
+    //   (result) => {
+    //     alert("Success!")
+    //     this.router.navigate(['/profile/reservations'])
+    //   },
+    //   (error : HttpErrorResponse) =>{
+    //     console.log(error)
+    //     alert(error.statusText)
+    //   }
+    // )
+
   }
 
-  constructor(){}
+  createStartDateStr(){
+    var timeStringArray = this.newReservationForm.value.time!.split(':')
+    var time= new Date();
+    time.setHours(+timeStringArray[0]);
+    time.setMinutes(+timeStringArray[1]);
+    return time.getHours+":"+time.getMinutes
+  }
+  createStartMinuteStringArray(){
+    var timeStringArray = this.newReservationForm.value.time!.split(':')
+    var time= new Date();
+    time.setHours(+timeStringArray[0]);
+    time.setMinutes(+timeStringArray[1]);
+    return time.getHours+":"+time.getMinutes
+  }
+  createEndDateStr(time: Date){
+    var endDate= new Date();
+    endDate.setHours(time.getHours()+2);
+    endDate.setMinutes(time.getMinutes())
+    return endDate.getHours+":"+endDate.getMinutes()
+  }
 
   ngOnInit(){
   }
--- restaurant/src/app/components/Tables/tables.component.ts
+++ restaurant/src/app/components/Tables/tables.component.ts
@@ -3,35 +3,92 @@ import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../Hero/hero.component';
 import { Table } from 'src/app/generated-api/model/table';
 import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
+import { TableModule} from 'primeng/table';
 import {  InputTextModule } from 'primeng/inputtext';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { TableService } from 'src/app/generated-api';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-tables',
   standalone: true,
-  imports: [CommonModule, HeroComponent,TableModule, ButtonModule, InputTextModule],
+  imports: [CommonModule, HeroComponent,TableModule, ButtonModule, InputTextModule, FormsModule, ReactiveFormsModule, DialogModule, DropdownModule],
   templateUrl: './tables.component.html',
   styleUrls: ['./tables.component.scss'],
 })
 export class TablesComponent implements OnInit{
   newTableFormOpen: boolean = false
   tables!: Table[]
+  newTableForm = new FormGroup({
+    number: new FormControl(0, [Validators.required]),
+    capacity: new FormControl(1, [Validators.required]),
+  })
+  tableStateOptions=[
+    Table.StatusEnum.Booked,
+    Table.StatusEnum.Free
+  ]
+
+  constructor(private tableService: TableService){}
+
+  onSubmit(){
+    var newTable: Table={
+      number:this.newTableForm.value.number!,
+      capacity: this.newTableForm.value.capacity!,
+    }
+
+    this.tableService.patchTable(newTable.number, newTable.capacity).subscribe(
+      (result)=>{
+        alert("Seccess!");
+        this.newTableFormOpen=false
+      }, (error: HttpErrorResponse)=>{
+        alert(error.statusText)
+      }
+    )
+
+  }
 
   ngOnInit(): void {
     this.tables = new Array<Table>()
+    this.tableService.listTables().subscribe(
+      (result)=>{
+        this.tables = result;
+      }, (error: HttpErrorResponse)=>{
+        alert(error.statusText)
+      }
+    )
   }
 
   onRowEditInit(table: Table){
 
   }
 
-  onRowEditSave(table: Table){}
+  onRowEditSave(table: Table){
+    this.tableService.patchTable(table.number, table.capacity).subscribe(
+      (result)=>{
+        alert("Seccess!");
+        this.newTableFormOpen=false
+      }, (error: HttpErrorResponse)=>{
+        alert(error.error)
+      }
+    )
+  }
 
   onRowEditCancel(table: Table, index: number){
 
   }
 
   newTableFormOpenBtnClick(){
-    this.newTableFormOpen= !this.newTableFormOpen
+    this.newTableFormOpen= true
+  }
+
+  deleteTable(table: Table){
+    this.tableService.deleteTable(table.number).subscribe(
+      (result)=>{}
+      ,(error:HttpErrorResponse)=>{
+
+      }
+    )
   }
 }
