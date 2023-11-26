e09c1406b3e099c189c54acb0e519f54a00a10da
dorinaorova <dorina.orova84@gmail.com>
2023-11-26 15:13:19 +0100

    user update

--- restaurant/src/app/components/ProfileView/ProfileProfile/profile-profile.component.ts
+++ restaurant/src/app/components/ProfileView/ProfileProfile/profile-profile.component.ts
@@ -1,15 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import { Order } from 'src/app/generated-api';
+import {  User, UserService } from 'src/app/generated-api';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-profile-profile',
   standalone: true,
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './profile-profile.component.html',
   styleUrls: ['./profile-profile.component.scss'],
 })
-export class ProfileProfileComponent {
-  order?: Order | null
+export class ProfileProfileComponent implements OnInit {
+  user: User={
+    name:"",
+    email:"",
+    mobil: "",
+    address: ""
+  }
+  userForm = new FormGroup({
+    name: new FormControl(this.user.name, [Validators.required]),
+    phone: new FormControl(this.user.mobil, [Validators.required]),
+    email: new FormControl(this.user.email, [Validators.required]),
+    address: new FormControl(this.user.address, [Validators.required]),
+  })
+
+  constructor(private userService: UserService){
+
+  }
+
+  ngOnInit(){
+    this.userService.getUserById(localStorage.getItem('userId')!).subscribe(
+      (result) => {
+        this.user = result;
+        console.log(result);
+      }
+    )
+  }
+
+  onSubmit(){
+    var user : User={
+      name: this.userForm.value.name!,
+      email: this.userForm.value.email!,
+      mobil: this.userForm.value.phone!,
+      address: this.userForm.value.address!
+    }
+    this.userService.updateUserById(localStorage.getItem('userId')!, user).subscribe(
+      (result)=>{
+        alert("Success!")
+      }
+    )
+  }
+  
 }
