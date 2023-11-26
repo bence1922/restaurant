0c5176167597a1cf35e3bb28f6dd8eec423efff1
botondklenk <klenk.botond@autsoft.hu>
2023-11-26 18:31:10 +0100

    regenerate api

--- restaurant/src/app/generated-api/api/table.service.ts
+++ restaurant/src/app/generated-api/api/table.service.ts
@@ -36,7 +36,7 @@ import { Configuration }                                     from '../configurat
 })
 export class TableService {
 
-    protected basePath = 'http://localhost:8080';
+    protected basePath = 'http://localhost:8080/api';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
     public encoder: HttpParameterCodec;
