159b8dfba3f8a849c15fd164733e77fec183adf1
dorinaorova <dorina.orova84@gmail.com>
2023-11-27 11:06:55 +0100

    profile-datas ok

--- restaurant/src/app/components/ProfileView/ProfileProfile/profile-profile.component.ts
+++ restaurant/src/app/components/ProfileView/ProfileProfile/profile-profile.component.ts
@@ -1,62 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import { Order, User, UserService } from 'src/app/generated-api';
-import { StoreUserService } from 'src/app/generated-api/api/store.service';
-import { Form, FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import {  User, UserService } from 'src/app/generated-api';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-profile-profile',
   standalone: true,
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './profile-profile.component.html',
   styleUrls: ['./profile-profile.component.scss'],
 })
 export class ProfileProfileComponent implements OnInit {
-  form: FormGroup | null = null;
-  order?: Order | null
-  
-  user?: User | null
+  user: User={
+    name:"",
+    email:"",
+    mobil: "",
+    address: "",
+    points: 0,
+  }
+  userForm = new FormGroup({
+    name: new FormControl(this.user!.name, [Validators.required]),
+    phone: new FormControl(this.user!.mobil, [Validators.required]),
+    email: new FormControl(this.user!.email, [Validators.required]),
+    address: new FormControl(this.user!.address, [Validators.required]),
+  })
 
-  constructor(
-    private storeUsderService: StoreUserService,
-    private userService: UserService,
-    private formBuilder: FormBuilder,
-  ) {}
+  constructor(private userService: UserService){
 
-  ngOnInit() {
-    this.user = this.storeUsderService.getUser();
-    this.user ={
-      id: "1",
-      name: "test",
-      email: "teszt",
-      mobil: "teszt",
-      address: "teszt",
-    }
-    this.form = this.formBuilder.group({
-      name: this.user.name,
-      email: this.user.email,
-      mobil: this.user.mobil,
-      address: this.user.address,
-    })
   }
 
-  changeUserData(){
-    const user = {
-      id: "1",
-      name: "test",
-      email: "teszt",
-      mobil: "teszt",
-      address: "teszt",
-    } as User;
-    this.userService.updateUserById(user.id!, user).subscribe(
-      (user: User) => {
-        this.user = user;
-      },
-      (error) => {
-        console.log(error);
+  ngOnInit(){
+    this.userService.getUserById(localStorage.getItem('userId')!).subscribe(
+      (result) => {
+        this.user = result;
       }
-    );
-
+    )
   }
+
+  onSubmit(){
+    var user : User={
+      name: this.userForm.value.name!,
+      email: this.userForm.value.email!,
+      mobil: this.userForm.value.phone!,
+      address: this.userForm.value.address!
+    }
+    this.userService.updateUserById(localStorage.getItem('userId')!, user).subscribe(
+      (result)=>{
+        alert("Success!")
+      }
+    )
+  }
+  
 }
