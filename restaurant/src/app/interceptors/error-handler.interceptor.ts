5ba01941200297ad88ea06594208183ae335b469
dorinaorova <dorina.orova84@gmail.com>
2023-11-27 11:25:17 +0100

    order+cart ok +errorHandlerInterceptor

--- restaurant/src/app/interceptors/error-handler.interceptor.ts
+++ restaurant/src/app/interceptors/error-handler.interceptor.ts
@@ -1,25 +1,15 @@
-import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
+  HttpInterceptorFn,
   HttpErrorResponse
 } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 
-@Injectable()
-export class ErrorHandlerInterceptor implements HttpInterceptor {
-
-  constructor() {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request)
-    .pipe(
-      catchError((error: HttpErrorResponse) =>{
-        let errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
-        return throwError(errorMsg);
-      })
-    )
-  }
-}
+export const errorInterceptor: HttpInterceptorFn = (request, next) => {
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      let errorMsg = `Error Code: ${error.status}, Message: ${error.error}`;
+      alert(errorMsg)
+      return throwError(errorMsg);
+    })
+  );
+};
